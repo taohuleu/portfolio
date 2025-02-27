2805bdffcd33fe9bad70c9a458075aee4be99e54
tao <taohuleu@gmail.com>
2025-02-27 14:03:24 +0100

    nam-fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,16 @@ function afficherImage() {
     }
     visible = !visible;
 }
+
+//Scroll Cards
+function scrollCards(direction) {
+    const container = document.querySelector('.tt');
+    const cardWidth = document.querySelector('.card').offsetWidth; // Largeur d'une carte
+    const scrollAmount = cardWidth * 2; // Défilement de 2 cartes à la fois (ajustez si nécessaire)
+
+    if (direction === -1) {
+        container.scrollBy({ left: -scrollAmount, behavior: 'smooth' }); // Défilement vers la gauche
+    } else if (direction === 1) {
+        container.scrollBy({ left: scrollAmount, behavior: 'smooth' }); // Défilement vers la droite
+    }
+}
